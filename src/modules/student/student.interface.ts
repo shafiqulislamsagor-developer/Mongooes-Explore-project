a057706b28148a9925269f33233b603be40b52c8
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-11 22:43:29 +0600

    sub schema and model create

--- src/modules/student/student.interface.ts
+++ src/modules/student/student.interface.ts
@@ -1,43 +1,39 @@
-
-
-
 export type Guardian = {
-    fatherName: string;
-    motherName: string;
-    fatherOccupation: string;
-    motherOccupation: string;
-    fatherContactNumber: string;
-    motherContactNumber: string;
-}
+  fatherName: string;
+  motherName: string;
+  fatherOccupation: string;
+  motherOccupation: string;
+  fatherContactNumber: string;
+  motherContactNumber: string;
+};
 
 export type UserName = {
-    firstName: string;
-    middleName: string;
-    lastName: string
-}
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
 
 export type LocalGuardian = {
-    name: string;
-    occupation: string;
-    contactNumber: string;
-    address: string;
-}
-
+  name: string;
+  occupation: string;
+  contactNumber: string;
+  address: string;
+};
 
 export type Student = {
-    id: string;
-    name: UserName;
-    gender: "male" | "female";
-    email: string;
-    avatar?: string;
-    dateOfBirth?: string;
-    contactNumber: string;
-    emergancyNumber: string;
-    bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
-    presentAddress: string;
-    permanentAddress: string;
-    guardian: Guardian;
-    localGuardian: LocalGuardian;
-    profileImage?: string;
-    isActive: 'active' | 'blocked';
-}
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  email: string;
+  avatar?: string;
+  dateOfBirth?: string;
+  contactNumber: string;
+  emergancyNumber: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: 'active' | 'blocked';
+};
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,14 @@ import config from './config';
 import mongoose from 'mongoose';
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port number ${config.port}`);
-        });
-    } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error}`);
-    }
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port number ${config.port}`);
+    });
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error}`);
+  }
 }
 
 main();
