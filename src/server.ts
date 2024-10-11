627d6ac35dd49ced4e71e4ba40ff0e9681f74033
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-11 18:59:01 +0600

    setup completed

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ async function main() {
     try {
         await mongoose.connect(config.database_url as string);
         app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`);
+            console.log(`Example app listening on port number ${config.port}`);
         });
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error}`);
