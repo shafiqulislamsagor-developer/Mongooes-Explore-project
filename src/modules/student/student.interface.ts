71246dde252c590354d1a9129a47d8e2c0c2ab5f
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-11 22:33:04 +0600

    student interface and schema create

--- src/modules/student/student.interface.ts
+++ src/modules/student/student.interface.ts
@@ -1,4 +1,4 @@
-import { Schema, model, connect } from 'mongoose';
+
 
 
 export type Guardian = {
@@ -10,22 +10,34 @@ export type Guardian = {
     motherContactNumber: string;
 }
 
+export type UserName = {
+    firstName: string;
+    middleName: string;
+    lastName: string
+}
+
+export type LocalGuardian = {
+    name: string;
+    occupation: string;
+    contactNumber: string;
+    address: string;
+}
+
 
 export type Student = {
     id: string;
-    name: {
-        firstName: string;
-        middleName: string;
-        lastName: string
-    };
+    name: UserName;
     gender: "male" | "female";
     email: string;
     avatar?: string;
-    dateOfBirth: string;
-    contactNumber: string | number;
-    emergancyNumber: string | number;
+    dateOfBirth?: string;
+    contactNumber: string;
+    emergancyNumber: string;
     bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
     presentAddress: string;
     permanentAddress: string;
-    guardian: Guardian
+    guardian: Guardian;
+    localGuardian: LocalGuardian;
+    profileImage?: string;
+    isActive: 'active' | 'blocked';
 }
